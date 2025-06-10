a1fea924f23a224e9094c63a13094949f32448c8
William <tiendat552@gmail.com>
2025-06-10 18:55:28 +0700

    refactor: remove unused namespaces in AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using LocalMartOnline.Models;
 using LocalMartOnline.Models.DTOs;
 using LocalMartOnline.Repositories;
