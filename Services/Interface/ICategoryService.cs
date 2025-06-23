283fa51a8c21a95a21c92c290fcb6cca957d6c09
Khang Nguyen <khangnvce170767@fpt.edu.vn>
2025-06-23 21:17:32 +0700

    Update Categoty Service

--- Services/Interface/ICategoryService.cs
+++ Services/Interface/ICategoryService.cs
@@ -8,13 +8,15 @@ namespace LocalMartOnline.Services.Interface
     public interface ICategoryService
     {
         Task<PagedResultDto<CategoryDto>> GetAllPagedAsync(int page, int pageSize);
+        Task<PagedResultDto<CategoryDto>> GetAllPagedAdminAsync(int page, int pageSize);
         Task<CategoryDto?> GetByIdAsync(string id);
         Task<CategoryDto> CreateAsync(CategoryCreateDto dto);
         Task<bool> UpdateAsync(string id, CategoryUpdateDto dto);
         Task<bool> DeleteAsync(string id);
         Task<bool> ToggleAsync(string id);
         Task<IEnumerable<CategoryDto>> SearchByNameAsync(string name);
+        Task<IEnumerable<CategoryDto>> SearchByNameAdmin(string name);
         Task<IEnumerable<CategoryDto>> FilterByAlphabetAsync(char alphabet);
-
+        Task<IEnumerable<CategoryDto>> FilterByAlphabetAdminAsync(char alphabet);
     }
 }
